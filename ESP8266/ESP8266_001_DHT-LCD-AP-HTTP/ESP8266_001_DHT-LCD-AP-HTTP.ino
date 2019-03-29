998809683f7aae3a6703106a003d626c7d11ef1f
Morpheus Hsieh <morpheus.hsieh@gmail.com>
2019-03-30 01:04:21 +0800

    commit

--- ESP8266/ESP8266_001_DHT-LCD-AP-HTTP/ESP8266_001_DHT-LCD-AP-HTTP.ino
+++ ESP8266/ESP8266_001_DHT-LCD-AP-HTTP/ESP8266_001_DHT-LCD-AP-HTTP.ino
@@ -4,12 +4,15 @@
 #include <Wire.h>
 
 #define BAUD_SERIAL   9600
-#define BAUD_ESP8266  9600
+#define BAUD_ESP8266  115200
 
 #define PRINT_DHT_SERIAL    false
 #define PRINT_DHT_LCD       true
 #define PRINT_WiFi_SERIAL   true
 #define PRINT_WiFi_LCD      true
+
+#define AP_SSID "ESP8266"
+#define AP_PSWD "1234test"
 
 #define LCD_DURATION        500
 unsigned long CurrentTimestamp = millis() - LCD_DURATION;
@@ -47,40 +50,6 @@ void setup()
   wifi_setting();
 }
 
-void loop() 
-{
-  if (millis() >= CurrentTimestamp + LCD_DURATION)
-  {
-    CurrentTimestamp = millis();
-    float h = DHT_SENSOR.readHumidity();
-    float t = DHT_SENSOR.readTemperature();
-    if (PRINT_DHT_SERIAL) printMonitor(h, t);
-    if (PRINT_DHT_LCD)    printLCD(h, t);
-  }
-
-  // send AT command to ESP8266-01 form console (serial)
-  if (Serial.available()) {
-    ESP8266.write(Serial.read());
-  }
-
-//  if (ESP8266.available()) {
-//    Serial.write(ESP8266.read());
-//  }
-
-  if ( ESP8266.available() ) {              // receive message from ESP8266-01
-    if ( ESP8266.find("+IPD,") ) {          // detect the client's request
-      String msg = "";
-      byte connID = ESP8266.read() - 48;    // client's connection ID
-      while( ESP8266.available() ) {        // collect client's request from the web browser
-        msg += (char)ESP8266.read();
-        delay(20);                          // the delay will let the message become more stable
-      }
-      sendHTML(connID, msg.c_str());        // send HTML message to client
-      Serial.flush();
-    }
-  }
-}
-
 void lcd_setting()
 {
   // 初始化 LCD，一行 16 的字元，共 2 行，預設開啟背光
@@ -104,16 +73,20 @@ void wifi_setting()
   Serial.println("\nESP8266 setup start...");
 
   ESP8266.begin(BAUD_ESP8266);
-  sendATcmd("AT+RST\r\n",            3000);
-  sendATcmd("AT+GMR\r\n",            1000);
-  sendATcmd("AT+CWMODE=2\r\n",       1000);
-  sendATcmd("AT+CWMODE?\r\n",        1000);
-  sendATcmd("AT+CIPMUX=1\r\n",       1000);
-  sendATcmd("AT+CIPSERVER=1,80\r\n", 3000);
-  sendATcmd("AT+CIFSR\r\n",          1000);
+  // sendATcmd("AT+GMR\r\n", 1000);
+  sendATcmd("AT+RST\r\n", 3000);
+  sendATcmd("AT+CWMODE=2\r\n", 1000);
+  // sendATcmd("AT+CWMODE?\r\n", 1000);
+  
+  char* cmd = "AT+CWSAP=\""AP_SSID"\",\""AP_PSWD"\",11,3\r\n";
+  Serial.print((char*)cmd);
+  sendATcmd(cmd, 1000);
 
+  sendATcmd("AT+CWLAP\r\n", 1000);
+  sendATcmd("AT+CIFSR\r\n", 1000);
+  sendATcmd("AT+CIPMUX=1\r\n", 1000);
+  sendATcmd("AT+CIPSERVER=1,80\r\n", 1000);
   Serial.println("\r\nServer styarted at port 80...");
-  delay(1000);
 }
 
 void printMonitor(float h, float t)
@@ -178,7 +151,6 @@ void sendATcmd(char *cmd, unsigned int delay)
 void sendHTML(byte connID,char* msg) 
 {
   extern int connCount; 
-  String html;
   char cipSend[128];
   char cipClose[128];
 
@@ -188,22 +160,58 @@ void sendHTML(byte connID,char* msg)
   // Read temperature as Celsius (the default)
   float t = DHT_SENSOR.readTemperature(); 
    
-  html += "<html>\n\r";
-  html += "<head>\n\r";
+  String html = "";
+  html += "<html><head>\n\r";
   html += "  <meta http-equiv=\"refresh\" content=\"10\">\n\r";
-  html += "  <title>From ESP8266-01</title>\n\r";
-  html += "</head>\n\r";
+  html += "  <title>From ESP-01</title>\n\r";
+  html += "</head>";
   html += "<body>\n\r";
-  html += "  <p>ClientMsg: "; html += msg; html += "</p>\n\r";
-  html += "  <p>Humi: "; html += h; html += "%</p>\n\r";
-  html += "  <p>Temp: "; html += t; html += "*C</p>\n\r";
-  html += "</body>";
-  html += "</html>";
-  
+  html += "  <p>ClientMsg: ";   html += msg; html += "</p>\n\r";
+  html += "  <p>Humidity: ";    html += h;   html += "%</p>\n\r";
+  html += "  <p>Temperature: "; html += t;   html += "*C</p>\n\r";
+  html += "</body>\n\r";
+  html += "</html>\n\r";
+
   Serial.println(html);
   sprintf(cipSend,"AT+CIPSEND=%d,%d\r\n",connID,html.length());
   sprintf(cipClose,"AT+CIPCLOSE=%d\r\n",connID);
-  sendATcmd(cipSend, 1000);
-  sendATcmd(html.c_str(), 1000);
-  sendATcmd(cipClose, 1000);
+
+  sendATcmd(cipSend,1000);
+  sendATcmd(html.c_str(),1000);
+  sendATcmd(cipClose,1000);
+}
+
+void loop() 
+{
+  if (millis() >= CurrentTimestamp + LCD_DURATION)
+  {
+    CurrentTimestamp = millis();
+    float h = DHT_SENSOR.readHumidity();
+    float t = DHT_SENSOR.readTemperature();
+    if (PRINT_DHT_SERIAL) printMonitor(h, t);
+    if (PRINT_DHT_LCD)    printLCD(h, t);
+  }
+
+  // send AT command to ESP8266-01 form console (serial)
+  if (Serial.available()) {
+    ESP8266.write(Serial.read());
+  }
+
+  // Receive message from ESP8266-01
+  if (ESP8266.available())
+  { 
+    // Detect the client's request            
+    if ( ESP8266.find("+IPD,") ) 
+    {
+      String msg = "";
+      byte connID = ESP8266.read() - 48;    // client's connection ID
+      while( ESP8266.available() ) {        // collect client's request from the web browser
+        msg += (char)ESP8266.read();
+        delay(20);                          // the delay will let the message become more stable
+      }
+      Serial.print("\r\nmsg: '"); Serial.print(msg); Serial.print("'");
+      sendHTML(connID, msg.c_str());        // send HTML message to client
+      Serial.flush();
+    }
+  }
 }
