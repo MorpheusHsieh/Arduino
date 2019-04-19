2242a53f4a127d051129b89606b46f311b7276fc
Morpheus Hsieh <morpheus.hsieh@gmail.com>
2019-04-19 21:34:54 +0800

    commit

--- ESP8266/ESP-01S_010_AutoConnect/ESP-01S_010_AutoConnect.ino
+++ ESP8266/ESP-01S_010_AutoConnect/ESP-01S_010_AutoConnect.ino
@@ -10,23 +10,21 @@ void setup()
 {
   Serial.begin(115200);
 
-  //WiFiManager
   WiFiManager wifiManager;
 
-  //reset saved settings
+  // Reset saved settings
   //wifiManager.resetSettings();
     
-  //set custom ip for portal
-
-  // IPAddress ip(10,0,1,1);
-  // IPAddress gateway(10,0,1,1);
-  // IPAddress subnet(255.255.255.0);
-  // wifiManager.setAPStaticIPConfig(ip, gateway, subnet);
+  // Set custom ip for portal
+  //IPAddress ip(10,0,1,1);
+  //IPAddress gateway(10,0,1,1);
+  //IPAddress subnet(255.255.255.0);
+  //wifiManager.setAPStaticIPConfig(ip, gateway, subnet);
 
   wifiManager.autoConnect("AutoConnectAP");
    
   //if you get here you have connected to the WiFi
-  Serial.println("connected...yeey :)");
+  Serial.println("WiFi connected ...");
 }
 
 void loop() {
